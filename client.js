02a06af8c36935f7cf66ca5c7f991554b134c158
Karolina Redden <dubajkaro@gmail.com>
2022-02-21 22:23:18 +0000

    move up, setTimeout and setInterval experiments, as per the task the code has been commented out

--- client.js
+++ client.js
@@ -11,8 +11,16 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {//listener for connect
-    console.log("Welcome, you are connected!");//Print a message to the screen when the connection is successfully established.
-    conn.write('Name: KR');//send initials to the snake
+    console.log("Welcome, you are connected!");//print a message to the screen when the connection is successfully established.
+    conn.write("Name: KR");//send initials to the snake
+    // conn.write("Move: up");//Sending the string as data to the server, snake moves up
+    // conn.write("Move: up");
+    // setTimeout(() => {
+    //   conn.write("Move: up");//setTimeout as per the "Experiments"
+    // }, 50);
+    // setInterval(() => {
+    //   conn.write("Move: up");//setInterval as per the "Experiments"
+    // }, 2000);//hits the wall hahahah
   });
 
   conn.on("data", (data) => {//listener for data, as a result we will see "you ded cuz you idled"
