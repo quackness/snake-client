7bda40f01aa26f61902061749e0efa617d15e93c
Karolina Redden <dubajkaro@gmail.com>
2022-02-21 20:03:53 +0000

    exporting connect function

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {//listener for connect
+    console.log("Welcome, you are connected!");
+  });
+  conn.on("data", (data) => {//listener for data, as a result we will see "you ded cuz you idled"
+    console.log(data);
+  });
+  return conn;
+};
+console.log("Connecting ...");
+//connect();// we do not need this call anymore. We are calling it in another file.
+
+module.exports = {
+  connect
+};
