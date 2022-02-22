fe662d2df62f55dacd0c5f31c90b5b2b8d70a3cc
Karolina Redden <dubajkaro@gmail.com>
2022-02-22 00:49:55 +0000

    Stdin Setup with ctr c

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
-const {connect} = require("./client")
+const {connect} = require("./client");
 const conn = connect();
+
+// setup interface to handle user input from stdin
+const handleUserInput = function(key) {//specified that the program should exit with a keyboard shortcut
+  if (key === '\u0003') {//ctr+c key
+    process.exit();//terminate
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);//callback
+  return stdin;
+};
+setupInput();
+
+
 
 
 
